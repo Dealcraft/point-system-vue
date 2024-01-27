d529120868fbc20715f0fdf2dbcc6fcbfcf79f6d
David <david.loh18.dl@gmail.com>
2024-01-27 14:15:41 +0100

    wrapper and prettier config

--- src/wrapper.js
+++ src/wrapper.js
@@ -1,5 +1,51 @@
-export function install() {
+import { PTM } from "point-system";
+
+export function install(Vue, options) {
 	if (install.installed) return;
+
+	let items = options.items;
+	delete options.items;
+	let ptm = new PTM(items, options);
+
+	Vue.config.globalProperties.$ptmHasItem = itemIdentifier => {
+		return ptm.hasItem(itemIdentifier);
+	};
+
+	Vue.config.globalProperties.$ptmBuyItem = itemIdentifier => {
+		return ptm.buyItem(itemIdentifier);
+	};
+
+	Vue.config.globalProperties.$ptmGetItem = itemIdentifier => {
+		return ptm.getItem(itemIdentifier);
+	};
+
+	Vue.config.globalProperties.$ptmChargeUser = amount => {
+		return ptm.chargeUser(amount);
+	};
+
+	Vue.config.globalProperties.$ptmSellItem = itemIdentifier => {
+		return ptm.sellItem(itemIdentifier);
+	};
+
+	Vue.config.globalProperties.$ptmBalance = ptm.balance;
+	Vue.config.globalProperties.$ptmUserItems = ptm.userItems;
+
+	Vue.config.globalProperties.$ptmItems = ptm.items;
+	Vue.config.globalProperties.$ptmSetItems = items => {
+		return (ptm.items = items);
+	};
+
+	Vue.config.globalProperties.$ptmUser = ptm.user;
+	Vue.config.globalProperties.$ptmVERSION = ptm.VERSION;
+
+	Vue.config.globalProperties.$ptmSetOptions = options => {
+		return (ptm.options = options);
+	};
+
+	Vue.config.globalProperties.$ptmSetCallback = callback => {
+		return (ptm.callback = callback);
+	};
+
 	install.installed = true;
 }
 
